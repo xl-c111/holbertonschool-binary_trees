2b510bbeb7c70f350e719eb2a17a3efd62f0387b
XIA0LING CUI <10430@holbertonstudents.com>
2025-04-07 21:40:24 +1000

    Update 9-binary_tree_height.c
    Binary tree height = longest path from root to a leaf, measured in edges
    empty tree: height = 0;
    single node: height = 0;
    Perfectly balanced tree: height = log₂(n);

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -14,14 +14,14 @@ size_t binary_tree_height(const binary_tree_t *tree)
 
 	if (tree == NULL)
 		return (0);
-	if (tree->left == NULL && tree->right == NULL)
+	if (tree->left == NULL && tree->right == NULL)     /* if the root node is a leaf, its height is 0 */
 		return (0);
 
-	left_height = binary_tree_height(tree->left);
+	left_height = binary_tree_height(tree->left);     /* post-order traversal, recursively compute the left and right subtree */
 	right_height = binary_tree_height(tree->right);
 
-	if (left_height >= right_height)
-		return (left_height + 1);
+	if (left_height >= right_height）              
+		return (left_height + 1);  /* max (left, right) is the tallest subtree; +1 means the edge connecting this node to its child */
 	else
 		return (right_height + 1);
 }
