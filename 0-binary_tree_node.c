dd86ac856bd96c0832824c166b2c6c462ccf0378
xl-c111 <10430@holbertonstudents.com>
2025-04-07 07:41:23 +0000

    creates a binary tree node

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -1,4 +1,5 @@
 #include <stdlib.h>
+#include <string.h>
 #include "binary_trees.h"
 /**
  * binary_tree_node - creates a binary tree node
@@ -8,6 +9,17 @@
  */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
+	binary_tree_t *newNode;
 
+	newNode = malloc(sizeof(binary_tree_t));
+
+	if (newNode == NULL)
+		return (NULL);
+
+	newNode->n = value;
+	newNode->parent = parent;
+	newNode->left = NULL;
+	newNode->right = NULL;
+	return (newNode);
 }
 
