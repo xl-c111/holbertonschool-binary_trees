da6a60f9e40bcf460b72ff3ef6950468139ce44d
XIA0LING CUI <10430@holbertonstudents.com>
2025-04-08 22:32:39 +1000

    Update 18-binary_tree_uncle.c
    calls find_sibling_func to find sibling of node->parent
    sibling of parent is uncle

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -30,5 +30,5 @@ binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
 	if (node == NULL || node->parent == NULL)
 		return (NULL);
-	return (binary_tree_sibling(node->parent));
+	return (binary_tree_sibling(node->parent));    /* node->parent points its parent node */
 }
