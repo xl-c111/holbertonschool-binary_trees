eee111cdbceb1a9e658f21982766c714d74f7dcb
XIA0LING CUI <10430@holbertonstudents.com>
2025-04-08 14:00:56 +1000

    Update 13-binary_tree_nodes.c
    nodes with at least 1 child == non- leaf nodes
    if the current node is neither NULL nor a leaf, it's a non-leaf node

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -5,7 +5,10 @@
  * binary_tree_nodes -  counts the nodes with at least 1 child in a binary tree
  * @tree: a pointer to the root node of the tree to count the number of nodes
  * Return: the number of the nodes with at least 1 child
- *         0 if tree is NULL
+ *         0 if the tree is NULL
+ * 
+ * Description: nodes with at least 1 child are non-leaf nodes. if the current
+ *              node is neither NULL nor a leaf, it should be a non-leaf node.
  */
 size_t binary_tree_nodes(const binary_tree_t *tree)
 {
