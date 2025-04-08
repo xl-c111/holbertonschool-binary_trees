c2f0b9be23fc0a38972c436a40531b3804ac4355
XIA0LING CUI <10430@holbertonstudents.com>
2025-04-08 21:58:53 +1000

    Update 15-binary_tree_is_full.c
    left_full = binary_tree_is_full(tree->left) recursively check if the left subtree is full. If it's full, return 1, not full, return 0.
    return (left_full && right_full) to check if both of them are full

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -18,9 +18,10 @@ int binary_tree_is_full(const binary_tree_t *tree)
 
 	if (tree->left != NULL && tree->right != NULL)
 	{
+		/* recursively check if left-and right-subtree is full, the result could be 1 or 0 */
 		left_full = binary_tree_is_full(tree->left);
-		right_full = binary_tree_is_full(tree->right);
-		return (left_full && right_full);
+		right_full = binary_tree_is_full(tree->right);     
+		return (left_full && right_full);    /* if both of them are full, returns 1, otherwise 0 */
 	}
 	return (0);
 }
