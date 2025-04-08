24e0fc76375c27b31372d41fe820692912b7f01a
XIA0LING CUI <10430@holbertonstudents.com>
2025-04-08 22:09:40 +1000

    Update 16-binary_tree_is_perfect.c
    perfect binary tree:
    all of its leaf node are on the same level
    all of its internal nodes have two children
    
    total_nodes = 2^{height + 1 } - 1 (height based on edges)
     total_nodes = 2^{height } - 1 (height based on nodes)

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -28,7 +28,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
 /**
  * binary_tree_size - measures the size of a binary tree
  * @tree: a pointer to the root node of the tree to measure the size
- * Return: the size of a binary tree, 0 if tree is NULL
+ * Return: the size of a binary tree, 0 if the tree is NULL
  */
 size_t binary_tree_size(const binary_tree_t *tree)
 {
@@ -43,6 +43,8 @@ size_t binary_tree_size(const binary_tree_t *tree)
  * @tree:  a pointer to the root node of the tree to check
  * Return: Always 1 (Success), otherwise 0
  *         0 if the tree is NULL
+ * Description: perfect binary tree: all of its internal nodes have two children;
+ *                                   all of its leaf nodes are on the same level(same height)
  */
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
@@ -56,8 +58,9 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 
 	height = binary_tree_height(tree);
 	count = binary_tree_size(tree);
-
-	if (count == ((1 << (height + 1)) - 1))
+        /* if a binary tree is perfect, means its total_nodes = (2^{height + 1} - 1) */
+	/* in this case, height is based on edges, height of a leaf is 0 */
+	if (count == ((1 << (height + 1)) - 1))    
 		return (1);
 	else
 		return (0);
